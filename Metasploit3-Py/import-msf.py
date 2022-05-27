1a293e855e55c0a14033eb875071a65a8fbb1811
Daniel <padillaortiz.daniel099@gmail.com>
2022-05-27 02:06:18 -0400

    adding metasploit folder

--- Metasploit3-Py/import-msf.py
+++ Metasploit3-Py/import-msf.py
@@ -5,9 +5,23 @@ from pymetasploit3.msfrpc import MsfRpcClient
 print('\nMake sure MSF is running on another tab.\n')
 print('In termial type:\n\n msfconsole \n load msgrpc [pass=yourpasswd] \n')
 
-print('Take note of MSGRPC Password.')
+print('Take note of MSGRPC Password.\n')
+
 PASSWORD = input('Enter your password: ')
 
 client = MsfRpcClient(PASSWORD, port=55552, ssl=False)
 
-print(client.modules.exploits)
+clientExploitsDB = client.modules.exploits
+print(len(clientExploitsDB))
+
+clientPayloadsDB = client.modules.payloads
+print(len(clientPayloadsDB))
+
+
+exploit = client.modules.use('exploit', 'unix/ftp/vsftpd_234_backdoor')
+
+print(exploit.description)
+
+print(exploit.options)
+
+print(exploit.missing_required)
