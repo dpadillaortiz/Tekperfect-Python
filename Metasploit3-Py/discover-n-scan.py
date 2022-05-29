aa089602d45abdc2748e3131c9d29cf89f43c456
Daniel <padillaortiz.daniel099@gmail.com>
2022-05-29 18:34:19 -0400

    started creating functions

--- Metasploit3-Py/discover-n-scan.py
+++ Metasploit3-Py/discover-n-scan.py
@@ -6,16 +6,40 @@ import subprocess as sp
 DISCOVER_FILE = 'discover-networks.txt'
 NMAP_FILE = 'nmap-recon.txt'
 
-# Get ip addres
-# Ask for ip range to scan
-# Run netdiscover
-# Run nmap scan 
+'''
+    1. Need to know my IP address
+        a. Use `ip address` to find out
+            - write to a file
+            - read and printed from a file
+    2. Discover networks
+        a. Ask for target ip range
+        b. Use `netdiscover` with target ip range
+            - needs to written to a file
+            - file needs to be read and printed out
+    3. Run nmap scan on an ip from discovered above
+        a. Ask for ip tager
+        b. use `nmap` on ip target 
+            - write to a file
+            - read from file and print it
+'''
 
-def getIpAddr():
+def cmdOutputToFile(command, file):
+    with open(file, 'w') as data:
+        sp.run(command, stdout=data, text=True)
+
+def printFile(file):
+    with open(file, 'r') as data:
+        display = data.read()
+    print(display)
+
+
+def ipAddr():
+    FILE = 'ip-address.txt'
+    CMD = ['ip', 'address']
     print('Grabbing IP information...')
-    sp.run(['ip', 'address'], stdout=sp.PIPE, text=True)
-    # Task: return the ip address
-
+    cmdOutputToFile(CMD, FILE)
+    printFile(FILE)
+    
 def discoverMyNet():
     print('Preparing to discover...')
     ipRange = input('Enter IP range: ')
@@ -31,4 +55,4 @@ def readFromFile(file):
         display = data.read()
     return display
 
-getIpAddr()
+ipAddr()
