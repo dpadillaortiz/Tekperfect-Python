8919095a08c11c74c78cb42efec875e6a1a49d45
Daniel <padillaortiz.daniel099@gmail.com>
2022-05-30 01:48:24 -0400

    Completed weather

--- Weather-Notifications/weather-app.py
+++ Weather-Notifications/weather-app.py
@@ -2,6 +2,7 @@
 
 import json
 import requests
+from twilio.rest import Client
 
 '''
 # Making sure requests works
@@ -9,14 +10,29 @@ x = requests.get('https://w3schools.com/python/demopage.htm')
 print(x)
 '''
 
-with open('ACCESS-KEY.json') as data_file:
+with open('credentials.json') as data_file:
     secret = json.load(data_file)
 
 baseURL = 'http://api.weatherstack.com'
 endpoint = 'current'
 query = 'Oakland'
-access_key = secret['key']
+access_key = secret['weather_key']
 apiReq = '{}/{}?access_key={}&query={}'.format(baseURL,endpoint, access_key, query)
-weather = requests.get(apiReq)
-print(weather.json())
+weather = requests.get(apiReq).text
+print(weather)
 
+weatherDict = json.loads(weather)
+
+accountID = secret['twilio_sid']
+authToken = secret['twilio_token']
+
+client = Client(accountID, authToken)
+txtMsg = 'The temperature is {}'.format(weatherDict['current']['temperature'])
+
+message = client.messages.create(
+    to = secret['to'],
+    from_ = secret['from'],
+    body = txtMsg
+)
+
+print(message)
