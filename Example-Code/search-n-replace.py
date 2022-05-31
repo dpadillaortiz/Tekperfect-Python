b46f83d256271501735f386b69a134837277b7e0
Daniel <padillaortiz.daniel099@gmail.com>
2022-05-31 18:25:27 -0400

    Adding example code

---
+++ Example-Code/search-n-replace.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+with open('example-text.txt','r') as file:
+    myFile = file.read()
+
+print(myFile)
+
+# More lines of code
+
---
+++ Example-Code/sed-example.sh
@@ -0,0 +1,9 @@
+
+
+
+echo 'This is the original file:'
+cat example-text.txt
+
+echo 'This is the file are running the command'
+sed 's/foo/bar/gI' example-text.txt
+
