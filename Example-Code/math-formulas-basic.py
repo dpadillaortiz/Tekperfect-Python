91d135fa44cc8accb92fd0673e5b5d840b18b410
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-06-23 20:40:52 -0700

    Added an outline to follow during the instructional.

--- Example-Code/math-formulas-basic.py
+++ Example-Code/math-formulas-basic.py
@@ -1,15 +1,19 @@
 #! /usr/bin/python3
 
-# Math Formulas
+# Intro to Comments
 
+# Math Formulas
+'''
 # Pythagoreom Theorem
 # c^2 = a^2 + b^2
 # -> c = sqrt(a^2 + b^2)
+'''
 
 # c represents the hypothenuse 
 # a, b represent the other two sides
 
 # Variables and Data Types
+- can only access variables declared before it i.e. above the line you're on
 aSide = 5
 bSide = 6
 
--- Example-Code/math-formulas-functions.py
+++ Example-Code/math-formulas-functions.py
@@ -1,26 +1,47 @@
 #!/usr/bin/python3
 
-
 # What if we wanted to do this for multiple inputs?
+
+# Intro to Functions
+# - talk about parameters
+# - in scope variables
+
 def pythm(a,b):
-    aSide = a
-    bSide = b
+   aSide = a
+   bSide = b
     
-    aBSide = aSide**2 + bSide**2
+   aBSide = aSide**2 + bSide**2
 
-    cSide = aBSide**(1/2)
+   cSide = aBSide**(1/2)
 
-    return cSide
+   # Talk about return
+   return cSide
 
+# note parameters should be a tuple
+
+# Introduce assert
+# - checks to test that a condition is met
+# - if not, raises an AssertionError with optional msg
 def slopeFormula(cord1, cord2):
-    xPoint1, yPoint1 = cord1
-    xPoint2, yPoint2 = cord2
+   assert type(cord1) == type(()) and type(cord2) == type(()), 'Expected a tuple'
+   
+   # unpacking a tuple 
+   xPoint1, yPoint1 = cord1
+   xPoint2, yPoint2 = cord2
 
-    rise = yPoint2 - yPoint1
-    run = xPoint2 - xPoint1
+   rise = yPoint2 - yPoint1
+   run = xPoint2 - xPoint1
 
-    slope = rise/run
+   slope = rise/run
 
-    return slope
+   return slope
+
+# Won't display anthing to us
+slopeFormula((1,2), (2,4))
+
+# Now it will but doesn't need to be assigned to variable
+# can be done by passing the function to print()
+x = slopeFormula((1,2), (2,4))
+print(x)
 
 
