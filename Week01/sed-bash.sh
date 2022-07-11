a8fe594c7d687c38058c6f45689b77f637b678e4
twinky <padillaortiz.daniel099@gmail.com>
2022-07-11 10:25:18 -0700

    roomFilter was not showing listings between min max. Removed the nested if and add it's condition to the parent conditional

--- Week01/sed-bash.sh
+++
@@ -1,9 +0,0 @@
-
-
-
-echo 'This is the original file:'
-cat sed-text.txt
-
-echo 'This is the file are running the command'
-sed 's/foo/bar/gI' sed-text.txt
-
--- Week01/sed-python.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-
-with open('sed-text.txt','r') as file:
-    myFile = file.read()
-
-print(myFile)
-
-# More lines of code
-
