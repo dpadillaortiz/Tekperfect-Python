c52c63a4a69136ffcdd9c55245efe1874442e523
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-07-12 22:20:05 -0700

    renamed -finish to -med; renamed -medium to -finish; renamed -med to -medium"

--- Week02/csv-python-medium.py
+++ Week02/csv-python-medium.py
@@ -3,156 +3,94 @@
 import sys
 import csv
 
-'''
-Simple csv writer
-- how to write a row to a csv
-- use 'a' to append to the file
-- using 'w' will overwrite the file contents
-'''
+#INPUT = f'Input-CSV/{sys.argv[1]}'
+#OUTPUT = f'Output-CSV/{sys.argv[2]}'
 
-def simpleWrite():
-   CSV_FILE = f'Sample-CSV/{sys.argv[1]}'  
-   with open(CSV_FILE, 'a') as csvfile:
-      writeToCSV = csv.writer(csvfile)
-      writeToCSV.writerow([1,2,3,4,5,6,7])
+def printFile(file):
+   with open(file, 'r') as f:
+      reader = csv.reader(f)
+      for row in reader:
+         print(row)
+def test_print_file(file):
+   printFile(file)
 
-INPUT = f'Input-CSV/{sys.argv[1]}'
-OUTPUT = f'Output-CSV/{sys.argv[2]}'
+def writeOneRow(file, mode, row):
+   with open(file, mode) as f:
+      writer = csv.writer(f)
+      writer.writerow(row)
+def test_write_one_row(file, mode, row):
+   writeOneRow(file, mode, row)
+   printFile(file)
 
-'''
-IF function
-Syntax: =IF(logical test, value if true, value if false)
+def writeManyRows(file, mode, rows):
+   with open(file, mode) as f:
+      writer = csv.writer(f)
+      writer.writerows(rows)
+def test_write_many_rows(file, mode, rows):
+   writeManyRows(file, mode, rows)
+   printFile(file)
 
-zillow.csv has 7 headers
-'''
+def concatenate(*args):
+   new = ''
+   for word in args:
+      assert type(word) == type(new), f'{word} is not a string'
+      new += word
+   return new
+def test_concatenate(*args):
+   c = concatentate(*args)
+   print(c)
 
-def withinBudget(price, budget):
-   assert type(price) == type(budget) == type(20), 'non int was passed to `withinBudget`'
-   if price <= budget:
-      return True
+def left(word, pos):
+   assert type(word) == type(''), f'{word} is not a string'
+   leftSide = word[:pos]
+   return leftSide
+def test_left(word, pos):
+   l = left(word, pos)
+   print(l)
 
-def budget(budget):
-   updatedRows = []
-   assert type(budget) == type(20), 'non int was passed to `budget`'
-   with open(INPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for row in csvReader:
-         if 'Index' in row:
-            updatedRows.append(row + ['Within Budget'])
-            continue
-         listingPrice = int(row[6]) 
-         if withinBudget(listingPrice, budget):
-            updatedRows.append(row + ['Yes'])
-         else:
-            updatedRows.append(row + ['No'])
-   
-   for row in updatedRows:
-      print(row)
+def right(word, pos):
+   assert type(word) == type(''), f'{word} is not a string'
+   rightSide = word[(pos-1):]
+   return rightSide
+def test_right(word, pos):
+   r = right(word, pos)
+   print(r)
 
-   with open(OUTPUT, 'w') as f:
-      csvWriter = csv.writer(f)
-      csvWriter.writerows(updatedRows)
-   
+def trim(word):
+   assert type(word) == type(''), f'{word} is not a string'
+   return word.strip()
+def test_trim(word):
+   t = trim(word)
+   print(t)
 
-'''
-CONCATENATE function
-Syntax: =CONCATENATE(text1, text2, ...)
-'''
+def unique(array):
+   assert type(array) == type([]), f'{array} is not a list'
+   return set(array)
+def test_unique(array):
+   u = unique(array)
+   print(u)
 
-def fullname(first, last):
-   assert type(first) == type(last) == type(''), 'non str was passed to `fullname`'
-   fullname = f'{first} {last}'
-   return fullname
-
-def username(f_initial, last_name):
-   assert type(f_initial) == type(last_name) == type(''), 'non str was passed to `username`'
-   user = f_initial + last_name
-   return user.lower()
-
-def email(user):
-   assert type(user) == type(''), 'non str was passed to `email`'
-   email = f'{user}@tekperfect.com'
-   return email.lower()
-
-# Introducing enumerate()
-def employee():
-   updatedRows = []
-   with open(INPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for line, row in enumerate(csvReader):
-         if line == 0:
-            updatedRows.append(row)
-         else:
-            first = row[0]
-            last = row[1]
-            user = username(first[0], last)
-            newData = [fullname(first, last), user, email(user)]
-            updatedRows.append(row + newData)
-   print(updatedRows)
-   for row in updatedRows:
-      print(row)
-   with open(OUTPUT, 'w') as f:
-      csvWriter = csv.writer(f)
-      csvWriter.writerows(updatedRows)
-
-   
-'''
-FILTER funciton
-Syntax: =FILTER(array, include, if empty)
-'''
-
-def budgetFilter(within_budget):
-   if within_budget == 'Yes':
-      return True
-
-def roomFilter(listing_rooms, min_rooms = None, max_rooms = None):
-   if max_rooms != None and (listing_rooms < min_rooms or listing_rooms > max_rooms):
-      return False
-   elif listing_rooms < min_rooms:
-      return False
+def sort(array, order):
+   assert type(array) == type([]), f'{array} is not a list'
+   assert type(order) == type(''), f'{order} is not a str'
+   assert order.lower() in ['a', 'd'], f'Order must be "a" for ascending or "b" for descending'
+   if order.lower() == 'a':
+      array.sort()
+      return array
    else:
-      return True
-   
-def applyBudgetFilter():
-   with open(OUTPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for line, row in enumerate(csvReader):
-         budgetCol = row[7]
-         if line == 0:
-            print(row)
-         elif budgetFilter(budgetCol):
-            print(row)
-          
-def applyRoomFilter(min = None, max = None):
-   with open(INPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for line, row in enumerate(csvReader):
-         listingRooms = row[2]
-         if line == 0:
-            print(row)
-         elif max != None:
-            if roomFilter(int(listingRooms), int(min), int(max)):
-               print(row)
-         elif roomFilter(int(listingRooms), int(min)):
-            print(row)
-           
+      array.sort(reverse=True)  
+      return array
+def test_sort(array, order):
+   s = sort(array, order)
+   print(s)
 
 if __name__ == '__main__':
-   #print('Adding the budget column')
-   #budget(200000)  
-   print('Employee Info')
-   employee()
-   #print('Houses under my budget')
-   #applyBudgetFilter()
-   #print('Houses with 3 rooms or less')
-   #applyRoomFilter(1,3)
+   print('Testing unique')
+   toBeUnique = ['a','a','apple','mate',1,1]
+   test_unique(toBeUnique)
 
-
-
-
-
-
-
-
-
-
+   print('Testing sort')
+   toBeSorted = ['star', 'wars', 'darth', 'vader']
+   test_sort(toBeSorted, 'a')
+   test_sort(toBeSorted, 'd')
+   
