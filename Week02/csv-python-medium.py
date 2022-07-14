cdaf59306c78c325f81598fd7c222413f7075c33
twinky <padillaortiz.daniel099@gmail.com>
2022-07-13 22:33:34 -0700

    updated -medium

--- Week02/csv-python-medium.py
+++ Week02/csv-python-medium.py
@@ -4,14 +4,19 @@ import csv
 import sys
 import csv_python_basic as cpb
 
-def printFile(file):
-   with open(file, 'r') as csvFile:
-      csvData = csv.reader(csvFile)
-      for row in csvData:
-         if len(row) > 1:
-            print(row)
 
+inputDir = 'Input-CSV/'
+outputDir = 'Output-CSV/'
+sampleDir = 'Sample-CSV/'
 
+'''
+Let's print one of the files we have
+In the main function:
+path = f'{sampleDir}{sys.argv[1]}'
+cpb.readFile(path)
+'''
+
+   
 '''
 ['   41', '    9', '   35', ' "N"', '     81', '   14', '   23', ' "W"', ' "Ravenna"', ' OH ']
 
@@ -20,46 +25,50 @@ This is the current output. We need to clean up the extra spaces.
 Task: Remove the extra spacing from the start and end of each item
 - Clean up any additional fluff
 '''
-def cleanOutput(file):
-   with open(file, newline='') as csvFile:
-      csvData = csv.reader(csvFile)
-      for row in csvData:
-         if len(row) <= 1:
-            continue
-         for i in range(len(row)):
-            row[i] = row[i].strip().strip('"')
-         print(row)
 
+def clean(file):
+   with open(file, 'r') as f:
+      reader = csv.reader(f)
+      # Introduce enumerate 
+      for index, row in enumerate(reader):
+         # Introduce range-len
+         for item in range(len(row)):
+            # Added .strip(") to remove the extra "
+            row[item] = cpb.trim(row[item]).strip('"')
+         print(row)
+             
 
 '''
 ['133', '145', '26', '7', '3', '1', '42', '0.36', '3059']
 
 This output has been cleaned up. Let's change it so that it writes to a file
 '''
+def writeClean(input, output):
+   cleanRows = []
+   with open(input, 'r') as f:
+      reader = csv.reader(f)
+      for index, row in enumerate(reader):
+         cleanRows.append(cleanRow(row))
+   print(cleanRows)
+   cpb.writeManyRows(output, 'w', cleanRows)
 
-def loopThru(row):
-   cleanRow = []
-   for i in range(len(row)):
-      cleanRow.append(row[i].strip().strip('"'))
-   return cleanRow
+# Can define functions anywhere
 
-FILE = f'Sample-CSV/{sys.argv[1]}'
-CLEANED = f'Input-CSV/{sys.argv[2]}'
-
-def cleanToFile(input, output):
-   with open(input, 'r') as csvRead:
-      with open(output, 'w') as csvWrite:
-      
-         csvToClean = csv.reader(csvRead)
-         csvToWrite = csv.writer(csvWrite)
-         
-         for row in csvToClean:
-            if len(row) <= 1:
-               continue
-            cleanRow = loopThru(row)
-            print(cleanRow)
-            csvToWrite.writerow(cleanRow)
+def cleanRow(row):
+   assert type(row) == type([]), 'Argument passed to cleanRow was not a list'
+   for col, item in enumerate(row):
+      row[col] = cpb.trim(item).strip('"')
+   return row
 
 if __name__ == '__main__':
-   print('Hello, World!')
-   cleanToFile(FILE, CLEANED)
+   path = f'{sampleDir}{sys.argv[1]}'
+   print("Original File")
+   cpb.readFile(path)
+
+   print("Cleaned File")
+   clean(path)
+
+   print("Writing to new file")
+   writeClean(path, f'{outputDir}{sys.argv[2]}')
+   print('Printing the new file')
+   cpb.readFile(f'{outputDir}{sys.argv[2]}')
