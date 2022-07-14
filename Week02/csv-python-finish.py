49f04972db51878c724ddb5507495616aa25d2e4
twinky <padillaortiz.daniel099@gmail.com>
2022-07-13 23:12:57 -0700

    Need to fix up the -finish file

--- Week02/csv-python-finish.py
+++ Week02/csv-python-finish.py
@@ -2,58 +2,46 @@
 
 import sys
 import csv
-import csv_python_medium as cpm
-
-'''
-Simple csv writer
-- how to write a row to a csv
-- use 'a' to append to the file
-- using 'w' will overwrite the file contents
-'''
-
-def simpleWrite():
-   CSV_FILE = f'Sample-CSV/{sys.argv[1]}'  
-   with open(CSV_FILE, 'a') as csvfile:
-      writeToCSV = csv.writer(csvfile)
-      writeToCSV.writerow([1,2,3,4,5,6,7])
-
-INPUT = f'Input-CSV/{sys.argv[1]}'
-OUTPUT = f'Output-CSV/{sys.argv[2]}'
+import csv_python_basic as cpb
 
 '''
 IF function
 Syntax: =IF(logical test, value if true, value if false)
-
-zillow.csv has 7 headers
 '''
+def budgetFilter(within_budget):
+   if within_budget == 'Yes':
+      return True
 
+def roomFilter(listing_rooms, min_rooms = None, max_rooms = None):
+   if max_rooms != None and (listing_rooms < min_rooms or listing_rooms > max_rooms):
+      return False
+   elif listing_rooms < min_rooms:
+      return False
+   else:
+      return True
+ 
 def withinBudget(price, budget):
    assert type(price) == type(budget) == type(20), 'non int was passed to `withinBudget`'
    if price <= budget:
       return True
 
-def budget(budget):
-   updatedRows = []
-   assert type(budget) == type(20), 'non int was passed to `budget`'
-   with open(INPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for row in csvReader:
-         if 'Index' in row:
-            updatedRows.append(row + ['Within Budget'])
-            continue
-         listingPrice = int(row[6]) 
-         if withinBudget(listingPrice, budget):
-            updatedRows.append(row + ['Yes'])
-         else:
-            updatedRows.append(row + ['No'])
-   
-   for row in updatedRows:
-      print(row)
 
-   with open(OUTPUT, 'w') as f:
-      csvWriter = csv.writer(f)
-      csvWriter.writerows(updatedRows)
-   
+def budget(file, budget):
+   assert type(budget) == type(20), 'non int was passed to `budget`'
+   budgetColumn = []
+   with open(file, 'r') as f:
+      reader = csv.reader(f)
+      for index, row in enumerate(reader):
+         listingPrice = row[-1]
+         if index == 0:
+            budgetColumn.append('Within Budget')
+         elif withinBudget(int(listingPrice), budget):
+            budgetColumn.append('Yes')
+         else:
+            budgetColumn.append('No')
+   print(budgetColumn)
+   cpb.writeOneRow(file, 'a', budgetColumn)
+
 
 '''
 CONCATENATE function
@@ -102,18 +90,7 @@ FILTER funciton
 Syntax: =FILTER(array, include, if empty)
 '''
 
-def budgetFilter(within_budget):
-   if within_budget == 'Yes':
-      return True
-
-def roomFilter(listing_rooms, min_rooms = None, max_rooms = None):
-   if max_rooms != None and (listing_rooms < min_rooms or listing_rooms > max_rooms):
-      return False
-   elif listing_rooms < min_rooms:
-      return False
-   else:
-      return True
-   
+  
 def applyBudgetFilter():
    with open(OUTPUT, 'r') as f:
       csvReader = csv.reader(f)
@@ -139,10 +116,16 @@ def applyRoomFilter(min = None, max = None):
            
 
 if __name__ == '__main__':
-   #print('Adding the budget column')
-   #budget(200000)  
-   print('Employee Info')
-   employee()
+   inputDir = 'Input-CSV/'
+   outputDir = 'Output-CSV/'
+   sampleDir = 'Sample-CSV/'
+
+   path = f'{outputDir}{sys.argv[1]}'
+   print('Adding the budget column')
+   budget(path,200000)  
+   cpb.readFile(path)   
+   #print('Employee Info')
+   #employee()
    #print('Houses under my budget')
    #applyBudgetFilter()
    #print('Houses with 3 rooms or less')
