8570d6f7c56e1dac58cd5e65048ebe4e9e6711fd
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-07-14 20:25:15 -0700

    getting error in -finish because the employees.csv file has extra spaces

--- Week02/csv-python-finish.py
+++ Week02/csv-python-finish.py
@@ -4,141 +4,86 @@ import sys
 import csv
 import csv_python_basic as cpb
 
-'''
-IF function
-Syntax: =IF(logical test, value if true, value if false)
-'''
-def budgetFilterde(within_budget):
-   if within_budget == 'Yes':
-      return True
+class Housing:
+   def withinBudget(price, budget):
+      assert type(price) == type(budget) == type(20), 'non int was passed to `withinBudget`'
+      if price <= budget:
+         return True
 
-def roomFilter(listing_rooms, min_rooms = None, max_rooms = None):
-   if max_rooms != None and (listing_rooms < min_rooms or listing_rooms > max_rooms):
-      return False
-   elif listing_rooms < min_rooms:
-      return False
-   else:
-      return True
- 
-def withinBudget(price, budget):
-   assert type(price) == type(budget) == type(20), 'non int was passed to `withinBudget`'
-   if price <= budget:
-      return True
+   def budgetColumn(file, budget):
+      assert type(budget) == type(20), 'non int was passed to `budget`'
+      updatedRows = []
+      with open(file, 'r') as f:
+         reader = csv.reader(f)
+         for index, row in enumerate(reader):
+            listingPrice = row[-1]
+            if index == 0:
+               updatedRows.append(row + ['Within Budget'])
+            elif withinBudget(int(listingPrice), budget):
+               updatedRows.append(row + ['Yes'])
+            else:
+               updatedRows.append(row + ['No'])
+      cpb.writeManyRows(file, 'w', updatedRows)
 
-def budgetFilter(file, budget):
-   assert type(budget) == type(20), 'non int was passed to `budget`'
-   with open(file, 'r') as f:
-      reader = csv.reader(f)
-      for index, row in enumerate(reader):
-         budgetCol = row[-1]
-         if index == 0:
-            print(row)
-         elif budgetCol == 'Yes':
-            print(row)
-   
-def budget(file, budget):
-   assert type(budget) == type(20), 'non int was passed to `budget`'
+   def budgetFilter(file, price, budget):
+      assert type(budget) == type(20), 'non int was passed to `budget`'
+      with open(file, 'r') as f:
+         reader = csv.reader(f)
+         for index, row in enumerate(reader):
+            if index == 0:
+               print(row)
+            elif withinBudget(price, budget):
+               print(row)
+
+class Employee:
+   def fullname(first, last):
+      assert type(first) == type(last) == type(''), 'non str was passed to `fullname`'
+      fullname = f'{first} {last}'
+      return fullname
+
+   def username(f_initial, last_name):
+      assert type(f_initial) == type(last_name) == type(''), 'non str was passed to `username`'
+      user = f_initial + last_name
+      return user.lower()
+
+   def email(user):
+      assert type(user) == type(''), 'non str was passed to `email`'
+      email = f'{user}@tekperfect.com'
+      return email.lower()
+
+   def additionalInfo(first, last):
+      fullname = Employee.fullname(first, last)
+      firstInitial = cpb.left(first, 1)
+      username = Employee.username(firstInitial, last)
+      email = Employee.email(username)
+      return [fullname, username, email]
+
+
+def writeEmployeeInfo(file):
    updatedRows = []
    with open(file, 'r') as f:
       reader = csv.reader(f)
       for index, row in enumerate(reader):
-         listingPrice = row[-1]
+         print(row)
+         print(row[0])
+'''
+         firstname = row[0]
+         lastname = row[1]
          if index == 0:
-            updatedRows.append(row + ['Within Budget'])
-         elif withinBudget(int(listingPrice), budget):
-            updatedRows.append(row + ['Yes'])
-         else:
-            updatedRows.append(row + ['No'])
-   cpb.writeManyRows(file, 'w', updatedRows)
-
-
-'''
-CONCATENATE function
-Syntax: =CONCATENATE(text1, text2, ...)
-'''
-
-def fullname(first, last):
-   assert type(first) == type(last) == type(''), 'non str was passed to `fullname`'
-   fullname = f'{first} {last}'
-   return fullname
-
-def username(f_initial, last_name):
-   assert type(f_initial) == type(last_name) == type(''), 'non str was passed to `username`'
-   user = f_initial + last_name
-   return user.lower()
-
-def email(user):
-   assert type(user) == type(''), 'non str was passed to `email`'
-   email = f'{user}@tekperfect.com'
-   return email.lower()
-
-# Introducing enumerate()
-def employee():
-   updatedRows = []
-   with open(INPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for line, row in enumerate(csvReader):
-         if line == 0:
             updatedRows.append(row)
          else:
-            first = row[0]
-            last = row[1]
-            user = username(first[0], last)
-            newData = [fullname(first, last), user, email(user)]
-            updatedRows.append(row + newData)
+            info = Employee.additionalInfo(firstname, lastname)
+            updatedRows.append(info)
    print(updatedRows)
-   for row in updatedRows:
-      print(row)
-   with open(OUTPUT, 'w') as f:
-      csvWriter = csv.writer(f)
-      csvWriter.writerows(updatedRows)
-
-   
 '''
-FILTER funciton
-Syntax: =FILTER(array, include, if empty)
-'''
-
-  
-def applyBudgetFilter():
-   with open(OUTPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for line, row in enumerate(csvReader):
-         budgetCol = row[7]
-         if line == 0:
-            print(row)
-         elif budgetFilter(budgetCol):
-            print(row)
-          
-def applyRoomFilter(min = None, max = None):
-   with open(INPUT, 'r') as f:
-      csvReader = csv.reader(f)
-      for line, row in enumerate(csvReader):
-         listingRooms = row[2]
-         if line == 0:
-            print(row)
-         elif max != None:
-            if roomFilter(int(listingRooms), int(min), int(max)):
-               print(row)
-         elif roomFilter(int(listingRooms), int(min)):
-            print(row)
-           
-
 if __name__ == '__main__':
    inputDir = 'Input-CSV/'
    outputDir = 'Output-CSV/'
    sampleDir = 'Sample-CSV/'
 
-   path = f'{outputDir}{sys.argv[1]}'
-   print('Adding the budget column')
-   budget(path,200000)  
-   cpb.readFile(path)
-   print('Filter only budget houses')
-   budgetFilter(path, 2000)
+   #Housing.budgetColumn(f'{inputDir}zillow.csv', 200000)
 
-
-
-
+   writeEmployeeInfo(f'{inputDir}employees.csv')
 
 
 
