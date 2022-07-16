d73c57c213cdde94206ec70967cc2515bbf06de6
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-07-16 11:33:14 -0700

    Finished rps

--- rockpaperscisors.py
+++ rockpaperscisors.py
@@ -3,23 +3,32 @@
 import random
 
 print('Welcome to Rock, Paper, Scissors!')
-
+print('Type "exit" to exit the game.')
 options = ['rock', 'paper', 'scissors']
 
-myChoice = input('Choose your option: ').lower()
-botChoice = random.choice(options)
-print(f'The computer chose: {botChoice}')
+while True:
+   myChoice = input('Choose your option: ').lower()
 
-win = 'You win!'
+   win = 'You win!'
+   if myChoice == 'exit':
+      print('Thank you for playing.')
+      break
+   if myChoice not in options:
+      print('Your options are rock, paper, scissors.')
+      continue
 
-if myChoice == 'rock' and botChoice == 'scissors':
-   print(win)
-elif myChoice == 'paper' and botChoice == 'rock':
-   print(win)
-elif myChoice == 'scissors' and botChoice == 'paper':
-   print(win)
-elif myChoice == botChoice:
-   print('Tied!')
-else:
-   print('You lose')
+
+   botChoice = random.choice(options)
+   print(f'The computer chose: {botChoice}')
+
+   if myChoice == 'rock' and botChoice == 'scissors':
+      print(win)
+   elif myChoice == 'paper' and botChoice == 'rock':
+      print(win)
+   elif myChoice == 'scissors' and botChoice == 'paper':
+      print(win)
+   elif myChoice == botChoice:
+      print('Tied!')
+   else:
+      print('You lose')
 
